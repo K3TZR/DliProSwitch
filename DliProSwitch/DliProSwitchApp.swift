fb2362bda15ac6b383d5aa77d083b73faf93212e
Douglas Adams <douglas.adams@me.com>
2023-01-11 14:50:41 -0500

    Initial Commit

---
+++ DliProSwitch/DliProSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DliProSwitchApp.swift
+//  DliProSwitch
+//
+//  Created by Douglas Adams on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DliProSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
